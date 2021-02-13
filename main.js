3211ea1cb7a4ef600fd5a016de1ae8409aa20ca9
eashan1627 <74692161+eashan1627@users.noreply.github.com>
2021-02-13 16:00:10 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("My first console project.");
+
+var n1="Eashan ";
+
+var n2="Singh";
+
+var n3=n1.concat(n2);
+console.log(n3);
+
+var n4=10;
+
+var n5=5;
+
+var n6= "The sum of 10 + 5 is " + (n4 + n5);
+console.log(n6);
+
+var n7="The difference of 10 - 5 is ".concat(n4-n5);
+console.log(n7);
+
+var n8=(n4*n5);
+console.log("The multiple of 10 x 5 is " + n8);
+
+var n9=(n4/n5);
+console.log("The division of 10 / 5 is " +  n9);
